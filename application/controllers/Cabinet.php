2e3309110637226bd34ad6cd089f3d59cb4ffdff
kharamanv <kharaman.v@gmail.com>
2016-08-07 18:30:55 +0300

    fixed some cart counter issues

--- application/controllers/Cabinet.php
+++ application/controllers/Cabinet.php
@@ -8,8 +8,8 @@ class Cabinet extends CI_Controller
     public function index()
     {
         $data['cart_count'] = ( ! empty($this->session->products)) ? count($this->session->products) : 0;
-        $this->load->view('header');
-        $this->load->view('cabinet/cabinet', $data);
+        $this->load->view('header', $data);
+        $this->load->view('cabinet/cabinet');
         $this->load->view('footer');
     }
 }
